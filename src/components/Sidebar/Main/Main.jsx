12fdcecc13b74f326f0ded33aea2597957f30b74
ismail-hepl <ismail.s@hepl.com>
2024-03-19 19:44:56 +0530

    Feat(Context): Initialized the context provider

--- src/components/Sidebar/Main/Main.jsx
+++ src/components/Sidebar/Main/Main.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import "./Main.css"
 import { assets } from '../../../assets/assets'
+import { Context } from '../../../context/Context'
 
 const Main = () => {
+
+    const { onSent, recentPrompt, showResult, loading, resultData, setInput, input } = useContext(Context)
+
     return (
         <div className='main'>
             <div className="nav">
@@ -10,36 +14,57 @@ const Main = () => {
                 <img src={assets.user_icon} alt="" />
             </div>
             <div className="main-container">
-                <div className="greet">
-                    <p><span>Hello, Mohamed</span></p>
-                    <p>How can I Help you today?</p>
-                </div>
-                <div className="cards">
-                    <div className="card">
-                        <p>Settle a Debate: How should you store bread?</p>
-                        <img src={assets.bulb_icon} alt="" />
-                    </div>
-                    <div className="card">
-                        <p>Suggest beautiful places to see on an upcoming road trip</p>
-                        <img src={assets.compass_icon} alt="" />
-                    </div>
-                    <div className="card">
-                        <p>Write code for a specific task, including edge cases</p>
-                        <img src={assets.code_icon} alt="" />
-                    </div>
-                    <div className="card">
-                        <p>Give me ideas for what to do with what's in this image?</p>
-                        <img src={assets.gallery_icon} alt="" />
-                    </div>
-                </div>
+                {!showResult
+                    ? <>
+                        <div className="greet">
+                            <p><span>Hello, Mohamed</span></p>
+                            <p>How can I Help you today?</p>
+                        </div>
+                        <div className="cards">
+                            <div className="card">
+                                <p>Settle a Debate: How should you store bread?</p>
+                                <img src={assets.bulb_icon} alt="" />
+                            </div>
+                            <div className="card">
+                                <p>Suggest beautiful places to see on an upcoming road trip</p>
+                                <img src={assets.compass_icon} alt="" />
+                            </div>
+                            <div className="card">
+                                <p>Write code for a specific task, including edge cases</p>
+                                <img src={assets.code_icon} alt="" />
+                            </div>
+                            <div className="card">
+                                <p>Give me ideas for what to do with what's in this image?</p>
+                                <img src={assets.gallery_icon} alt="" />
+                            </div>
+                        </div>
+                    </> 
+                    : <div className="result">
+                        <div className="result-title">
+                            <img src={assets.user_icon} alt="" />
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className="result-data">
+                            <img src={assets.gemini_icon} alt="" />
+                            {loading 
+                                ? <div className="loader">
+                                    <hr />
+                                    <hr />
+                                    <hr />
+                                </div> 
+                                : <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                            }
+                        </div>
+                    </div> 
+                    }
 
                 <div className="main-bottom">
                     <div className="search-box">
-                        <input type="text" placeholder='Enter a prompt here' />
+                        <input type="text" placeholder='Enter a prompt here' onChange={(e) => setInput(e.target.value)} value={input} />
                         <div>
                             <img src={assets.gallery_icon} alt="" />
                             <img src={assets.mic_icon} alt="" />
-                            <img src={assets.send_icon} alt="" />
+                            <img src={assets.send_icon} alt="" onClick={() => onSent()} />
                         </div>
                     </div>
                     <p className="bottom-info">Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps</p>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,12 +1,39 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import runChat from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
 
+    const [input, setInput] = useState("");
+    const [recentPrompt, setRecentPrompt] = useState("");
+    const [prevPrompts, setPrevPrompts] = useState([]);
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData, setResultData] = useState("");
+
+    const onSent = async () => {
+        setResultData("");
+        setLoading(true);
+        setShowResult(true);
+        setRecentPrompt(input);
+        const response = await runChat(input);
+        setResultData(response);
+        setLoading(false);
+        setInput("");
+    }
 
     const contextValue = {
-
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput
     }
 
     return (
